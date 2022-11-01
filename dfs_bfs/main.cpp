3fed3a3219d94aa85b7f1bde375225a5d7ebbeab
luiz826 <luiz.costa@academico.ufpb.br>
2022-10-31 21:29:42 -0300

    Add bfs and dfs iteractive

--- dfs_bfs/main.cpp
+++ dfs_bfs/main.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <list>
 #include <stack>
 #include <fstream>
 
 using namespace std;
 
 int t = 0;
-queue<int> F;
-stack<int> P;
+
+stack<int> S;
+queue<int> Q;
 
 
 class Graph {
     public:
         int lenV;
-        string textFile;
-        vector<int> *adjList;
+        list<int> *adjList;
         vector<int> *parent;
         vector<int> *level;
         vector<int> *L;
 
-        Graph(string textFile);
+        Graph(int lenV);
         
         void dfs(int v);
         void bfs(int v);
-        void initGraph(void);
         void fillIndexAndParent(void);
 };
 
-Graph::Graph(string textFile) {
-    this -> textFile = textFile;
+Graph::Graph(int lenV) {
     this -> lenV;
 
-    adjList = new vector<int>[lenV];
+    adjList = new list<int>[lenV];
     L = new vector<int>[lenV];
     parent = new vector<int>[lenV];
     level = new vector<int>[lenV];
@@ -46,122 +45,88 @@ void Graph::fillIndexAndParent(void) {
 }
 
 
-void Graph::initGraph(void) {
+
+void Graph::bfs(int v) {
+    Q.push(v);
+    level[v].push_back(0);
+    parent[v].push_back(-1);
+    while (!Q.empty()) {
+        int u = Q.front();
+        Q.pop();
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+            if (level[*i].empty()) {
+                level[*i].push_back(level[u][0] + 1);
+                parent[*i].push_back(u);
+                Q.push(*i);
+            } else if (level[*i][0] == level[u][0] + 1) {
+                cout << *i << " IS UNCLE OF " << v << " <GREEN> " << endl;   
+           } 
+        }
+    }
+}
+
+
+void Graph::dfs(int v) {
+    S.push(v);
+    while (!S.empty()) {
+        int u = S.top();
+        S.pop();
+        if (L[u].empty()) {
+            t++;
+            L[u].push_back(t);
+        }
+        for (auto i = adjList[u].begin(); i != adjList[u].end(); i++) {
+            if (L[*i].empty()) {
+                parent[*i].push_back(u);
+                S.push(*i);
+            }
+        }
+        if (L[u].size() == 1) {
+            t++;
+            L[u].push_back(t);
+            // F.push(t);
+            // P.push(u);
+        }
+    }
+}
+
+int main(void) {       
     ifstream graphFile;
     int isConected;
 
-    graphFile.open(textFile);
+    graphFile.open("instance.txt");
 
-    // number of vertices
-    graphFile >> lenV; 
-     
-    for (int i=1; i< lenV+1; i++) {
-        for (int j = i; j < lenV; j++) {
+    
+
+    Graph g(8);
+    g.fillIndexAndParent();
+    cout << g.lenV << endl;
+    graphFile >> g.lenV;
+    cout << g.lenV << endl;
+    for (int i = 1; i < g.lenV+1; i++) {
+    
+        for (int j = 1; j < g.lenV+1; j++) {
+        
             graphFile >> isConected;
             if (isConected) {
-                adjList[i].push_back(j);
-                adjList[j].push_back(i);    
+                cout << i << " -> " << j << endl;
+                g.adjList[i].push_back(j);
             }
             
         }
         cout << endl;
-    }  
-
+    }
 
     graphFile.close();
-}
 
-void Graph::dfs(int v) {
-    while (!F.empty()) {
-        int v = F.front();
-        F.pop();
-        
-        vector<int>::iterator w;
-        
-        for (w = adjList[v].begin(); w != adjList[v].end(); w++) {
-            if (L[*w].back() == 0) {
-                parent[*w].push_back(v);
-                level[*w].push_back(level[v].back()+1);
-                t += 1;
-                L[*w].push_back(t);
-                F.push(*w);
-            } 
-            else if (level[*w].back() == level[v].back()) {
-                if (parent[*w].back() == parent[v].back()) {
-                    cout << *w << "IS BROTHER OF " << v << " <RED> " << endl;
-                }
-                else {
-                    cout << *w << "IS COUSIN OF " << v << " <ORANGE> " << endl;    
-                }
-            }
-            else if (level[*w].back() == level[v].back()+1){
-                cout << *w << "IS UNCLE OF " << v << " <GREEN> " << endl;    
-            }
+    g.bfs(1);
+    g.dfs(1);
 
+    for (int a = 1; a < g.lenV+1; a++) {
+        cout << a << " ";
+        for (auto w = g.adjList[a].begin(); w != g.adjList[a].end(); w++) {
+            cout << *w << " ";
         }
-    }
-}           
-
-void Graph::bfs(int v) {
-    while (!P.empty()) {
-        int v = P.top();
-        P.pop();
-        
-        vector<int>::iterator w;
-        
-        for (w = adjList[v].begin(); w != adjList[v].end(); w++) {
-            if (L[*w].back() == 0) {
-                parent[*w].push_back(v);
-                level[*w].push_back(level[v].back()+1);
-                t += 1;
-                L[*w].push_back(t);
-                P.push(*w);
-                cout << v << "IS FATHER OF " << *w << " <RED> " << endl;
-            } 
-            else if (level[*w].back() == level[v].back()) {
-                if (parent[*w].back() == parent[v].back()) {
-                    cout << v << "IS BROTHER OF " << *w << " <RED> " << endl;
-                }
-                else {
-                    cout << v << "IS COUSIN OF " << *w << " <ORANGE> " << endl;    
-                }
-            }
-            else if (level[*w].back() == level[v].back()+1){
-                cout << v << "IS UNCLE OF " << *w << " <GREEN> " << endl;    
-            }
-
-        }
-    }
-}    
-
-
-
-int main(void) {       
-    // int len_v;
-    // int a;
-    // cin >> len_v;
-    // cout << "N " << len_v << endl;
-
-    // for (int i=1; i< len_v+1; i++) {
-    //     for (int j = i; j < len_v; j++) {
-    //         cin >> a;
-    //         cout << "(" << i << "," << j << ") -> " << a << " | ";
-    //     }
-    //     cout << endl;
-    // }
-    // int v = 0;
-
-    Graph g("instance.txt");
-    g.fillIndexAndParent();
-    g.initGraph();
-    cout << g.lenV << endl;
-    cout << g.adjList << endl;
-    // g.level[v].push_back(0);
-    // t += 1;
-    // g.L[v].push_back(t);
-    // F.push(v);
-    // g.dfs();
-    // g.bfs();
-    
-    
+        cout << endl;
+    }   
 }
